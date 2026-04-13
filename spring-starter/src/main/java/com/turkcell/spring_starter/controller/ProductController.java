ef74c503b9c05373cf9b2d64258474120f12f669
Muhammed <arslanmuhammed@outlook.com.tr>
2026-04-13 20:44:17 +0300

    spring actuator

--- spring-starter/src/main/java/com/turkcell/spring_starter/controller/ProductController.java
+++ spring-starter/src/main/java/com/turkcell/spring_starter/controller/ProductController.java
@@ -1,8 +1,12 @@
 package com.turkcell.spring_starter.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,29 +14,34 @@ import org.springframework.web.bind.annotation.RestController;
 import com.turkcell.spring_starter.model.Product;
 
 @RestController 
-@RequestMapping("/api/product") // localhost:8080/api/product -> ProductController
-// Bu class bir rest controllerdır, içini uygulama başladığında tara, http->function 
-// tanımlarını al
+@RequestMapping("/api/product")
 public class ProductController {
-    // Kullanıcı ne zaman /api/product alanına istek atarsa -> cevap bu fonksiyondan dönen cevap olsun
-    // /api/product -> sayHi(); matchle
-    // HTTP Method -> GET,POST,PUT,DELETE,PATCH...
-    @GetMapping("") // controllerın uzantısı + getin uzantısı -> /api/product
-    public String sayHi(String name, int age) {
-        return "Hi " + name + " yaşınız: " + age;
-    }
-    // Controllerın uzantısı + get'in uzantısı -> /api/product/hello/{name}
-    @GetMapping("hello/{name}/{age}")
-    public String sayHello(@PathVariable String name, @PathVariable int age) {
-        return "Hello " + name + " yaşınız: " + age;
+
+    private List<Product> products = new ArrayList<>();
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return products;
     }
 
-    @PostMapping
-    public Product add(@RequestBody Product product){ // Json->Java objesine
-        // isim 1 haneden uzun mu?
-        // fiyat..
-        // DB'e kaydet..
-        return product;
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable int id) {
+        return products.stream()
+                .filter(product -> product.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+
+    @PostMapping 
+    public void createProduct(@RequestBody Product product){
+        products.add(product);
+    }
+
+    @PutMapping("/{id}")
+    public void updateProduct(@PathVariable int id, @RequestBody Product updatedProduct) {
+        products.removeIf(product -> product.getId() == id);
+        updatedProduct.setId(id);
+        products.add(updatedProduct);
     }
 
 }
